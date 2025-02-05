0e1604dc2bd6b3cd0acd44b2fce06e48ee4f9fab
byOscarPINE <emanuelsant768@gmail.com>
2025-02-05 13:32:44 -0600

    Modal

--- src/app/components/modal-chido/modal-chido.component.ts
+++ src/app/components/modal-chido/modal-chido.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, 
+  ModalController, IonIcon, IonList, IonInput, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { close } from 'ionicons/icons';
 
 @Component({
   selector: 'app-modal-chido',
   templateUrl: './modal-chido.component.html',
   styleUrls: ['./modal-chido.component.scss'],
+  standalone: true,
+  imports: [IonLabel, HeaderComponent, IonContent, IonHeader, 
+    IonTitle, IonToolbar, IonButton, IonIcon, IonItem, IonList, IonInput],
 })
 export class ModalChidoComponent  implements OnInit {
 
-  constructor() { }
+  private modalCtrl = inject(ModalController);
+
+  constructor() {
+    addIcons({close});
+   }
+
+  async closeModal(){
+    await this.modalCtrl.dismiss();
+  }
 
   ngOnInit() {}
 
--- src/app/pages/modal/modal.page.ts
+++ src/app/pages/modal/modal.page.ts
@@ -1,26 +1,30 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, ModalController } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, ModalController, IonIcon } from '@ionic/angular/standalone';
 import { HeaderComponent } from "../../components/header/header.component";
 import { ModalChidoComponent } from 'src/app/components/modal-chido/modal-chido.component';
+import { addIcons } from 'ionicons';
+import { close } from 'ionicons/icons';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.page.html',
   styleUrls: ['./modal.page.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent]
+  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, IonIcon]
 })
 export class ModalPage implements OnInit {
 
   private modalCtrl = inject(ModalController);
 
-  constructor() { }
+  constructor() {
+   }
 
   async openModal(){
     const modal = await this.modalCtrl.create({
-      component: ModalChidoComponent
+      component: ModalChidoComponent,
+      initialBreakpoint: 0.8,
     });
     await modal.present();
   }
